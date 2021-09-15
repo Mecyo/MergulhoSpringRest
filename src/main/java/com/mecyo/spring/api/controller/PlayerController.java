2ca8e0875af4a704b8e01c49a47757e7c81b6168
Emerson Santos <emerson.santos@invillia.com>
2021-09-15 01:03:06 -0300

    Novo nome do host

--- src/main/java/com/mecyo/spring/api/controller/PlayerController.java
+++ src/main/java/com/mecyo/spring/api/controller/PlayerController.java
@@ -25,7 +25,7 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
-@CrossOrigin(origins = {"http://localhost:8081", "https://curso-msr.000webhostapp.com"})
+@CrossOrigin(origins = {"http://localhost:8081", "https://curso-msr.000webhostapp.com", "https://torneio-de-clash.000webhostapp.com"})
 @RequestMapping("/players")
 public class PlayerController {
 	
