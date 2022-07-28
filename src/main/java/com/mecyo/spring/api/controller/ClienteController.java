a7ffe1b200a192a39ae484b95d560a17c6c964d0
Emerson Santos <ejsmao@hotmail.com>
2022-07-28 04:24:59 -0300

    Revert "Enabling CORS"
    This reverts commit b9c5a4af144688d24a06fd6407f257c41f9be51d.

--- src/main/java/com/mecyo/spring/api/controller/ClienteController.java
+++ src/main/java/com/mecyo/spring/api/controller/ClienteController.java
@@ -6,7 +6,6 @@ import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +28,6 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
-@CrossOrigin
 @RequestMapping("/clientes")
 public class ClienteController {
 	
--- src/main/java/com/mecyo/spring/api/controller/PlayerController.java
+++ src/main/java/com/mecyo/spring/api/controller/PlayerController.java
@@ -9,7 +9,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +30,6 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
-@CrossOrigin
 @RequestMapping("/players")
 public class PlayerController {
 
