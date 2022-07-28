459f0f0621703aed3b6b75231639481275080036
Emerson Santos <ejsmao@hotmail.com>
2022-07-28 04:34:56 -0300

    Test CORS

--- src/main/java/com/mecyo/spring/security/CorsConfiguration.java
+++ src/main/java/com/mecyo/spring/security/CorsConfiguration.java
@@ -11,6 +11,7 @@ public class CorsConfiguration implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+        		.allowedOrigins("http://localhost:8080")
                 .allowedMethods("*");
     }
 }
