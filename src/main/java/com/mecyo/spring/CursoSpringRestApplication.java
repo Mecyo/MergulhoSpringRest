72d9f74d75da5813fdcb704fdde88f0976bcc20d
Emerson Santos <ejsmao@hotmail.com>
2022-07-28 05:07:25 -0300

    Test new configuration CORS

--- src/main/java/com/mecyo/spring/CursoSpringRestApplication.java
+++ src/main/java/com/mecyo/spring/CursoSpringRestApplication.java
@@ -2,6 +2,9 @@ package com.mecyo.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CursoSpringRestApplication {
@@ -10,4 +13,15 @@ public class CursoSpringRestApplication {
 		SpringApplication.run(CursoSpringRestApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+
+	    return new WebMvcConfigurer() {
+	        @Override
+	        public void addCorsMappings(CorsRegistry registry) {
+	            registry.addMapping("/**")
+	                    .allowedOrigins("*");
+	        }
+	    };
+	}
 }
